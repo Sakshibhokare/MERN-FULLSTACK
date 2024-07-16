919989384ca4dbf1ea7ed16327b09e842e596ff6
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-16 15:42:37 +0530

    Promises Revision

--- week2/2.1-revisionPromiseAsync.js
+++ week2/2.1-revisionPromiseAsync.js
@@ -3,3 +3,32 @@
 // Promises 
 // Js functions (map, filter) 
 // Assignments 
+
+// Async Functions 
+function onDone(){
+    console.log("Hey there");
+}
+
+setTimeout(onDone, 2000);
+
+console.log("I am the first")
+
+// 2nd example
+//readFile is a async function 
+// reading a file 
+
+
+function promisified(duration){
+    const p= new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("inside a Promise")
+        },duration)
+    })
+    return p;
+}
+
+let ans=promisified(3000);
+
+ans.then(function(){
+console.log(ans)
+})
