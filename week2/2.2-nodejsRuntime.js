04c35e4c95a86617595de519fbc4771f5f6b2c9a
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-16 17:50:54 +0530

    learnings

---
+++ week2/2.2-nodejsRuntime.js
@@ -0,0 +1,23 @@
+//HTTP
+//Nodejs and its runtime 
+//Backend communication protocols
+//Express basic to advance
+//routes, status codes
+
+
+// what is ECMAScript, Javascript, nodejs, Bun 
+// 1. ECMAScript: is the specification on which the javascript is based 
+// 2. Javascript: Javascript is a scripting language 
+// 3. Nodejs: Backend framework 
+// 4.Bun: bun is much faster than nodejs, it is written in zig 
+
+
+// What to do with nodejs 
+// create clis (command line interface )
+// create a video player 
+// create a game 
+// create an HTTP Server
+
+
+// What is an HTTP Server : this is a sort of protocol to communicate
+
