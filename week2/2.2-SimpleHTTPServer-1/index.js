60cf6eec517a33005edb5a4d464ca907fe40b064
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-17 22:59:45 +0530

    syntax

--- week2/2.2-SimpleHTTPServer-1/index.js
+++ week2/2.2-SimpleHTTPServer-1/index.js
@@ -4,8 +4,20 @@
 //npm install express 
 const express = require("express");
 const port=3000;
-const app = express();
+const app = express(); //initializing instance of app
 
 app.get('/', function(req, res){
-    res.send('')
+    res.send('responding')
 })
+
+app.listen(port);
+
+// ####################################################
+// create a todo app that lets users store todos on the server 
+// try to create a http server from scratch in c 
+// create an http server in rust using actix-web 
+// create an http server in golang using the gurrila framework 
+// spring boot java 
+// ####################################################
+
+
