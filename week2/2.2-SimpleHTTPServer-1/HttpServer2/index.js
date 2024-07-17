a5c3711a1506ba61d2c1a5eddf7f69f8f169604e
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-17 23:13:08 +0530

    get request complete

--- week2/2.2-SimpleHTTPServer-1/HttpServer2/index.js
+++ week2/2.2-SimpleHTTPServer-1/HttpServer2/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/router", (req, res)=>{
+    //header body query parameters 
+    //do machine leaning model
+    res.json({
+        name:"sakshi",
+        age:21
+    })
+})
+
+
 app.get("/",(req, res)=>{
     res.send("Http Server 2");
 })
