6a29524f2fcd5435c571b036f67eb41b1c5515d8
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-22 18:11:21 +0530

    fetch and post methods

--- week2/2.2-SimpleHTTPServer-1/HttpServer2/index.js
+++ week2/2.2-SimpleHTTPServer-1/HttpServer2/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
+//body-parser
+//2 install body parser and import it 
+const bodyParser = require("body-parser")
+//middlewares
+app.use(bodyParser.json());
+//this will extract the json and put instead of req.body, the request must be post 
+app.post("/",(req, res)=>{
+    console.log(req.body);
+    res.send("Http Server 3");
+})
+
+
+//3 use nodemon 
+
+
+
 
 app.get("/router", (req, res)=>{
     //header body query parameters 
@@ -13,8 +29,25 @@ app.get("/router", (req, res)=>{
 
 
 app.get("/",(req, res)=>{
-    res.send("Http Server 2");
+    console.log(req.body);
+    res.send("Http Server!!!");
 })
+
+
+//post request 
+app.post('/conversations',(req,res)=>{
+    console.log(req.headers)
+    res.send({
+        msg:"2+2=4"
+    })
+})
+
+
+//fetch 
+// suppose we a Button, on clicking on it something should happen, then the fetch method will fetch the data which is required to display by clicking on it 
+
+
+
 
 app.listen(port, ()=>{
     console.log(`app is running on port ${port}`)
