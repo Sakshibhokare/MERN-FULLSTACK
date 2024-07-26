45c6d3ca317a1fcc2f59a2706504aa537f181771
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-26 15:02:20 +0530

    git and github

--- week2/2.7-gitGitHub.js
+++ week2/2.7-gitGitHub.js
@@ -34,3 +34,40 @@
 // create a SSh key on git bash 
 // put the key on github setting 
 
+// github flow - write code -> commit code changes -> pull request 
+// local git workflow - write code -> stage code changes git add -> commit changes git push
+
+
+
+// git branching: master -> develop -> topic
+// create new branch 
+// git branch - current branch
+// git checkout -b feature_update_file / create new branch 
+// git checkout main / go back to main
+
+// making changes into the file 
+// add 
+// commit 
+// need to merge the branch / git merge feature_update_file
+// need to set upstream / git push -u origin feature_update_file
+
+// then go to github and merge the pull request 
+// add details 
+// create pull request 
+
+// git diff - to see changes 
+
+
+// git commands to resolve conflicts 
+// git log --merge: produce the list of commits that are causing the conflict 
+// git diff: Identify the difference between the states repositories or files 
+// git checkout: used to undo the changes made to the file or for changing branches 
+// git reset: use to go back to the working stage 
+// git reset --mixed: used to undo changes to the working directory and staging area 
+// git merge --abort: helps in existing the merge process and returning back to the state before the merging began 
+// git reset: used at the time of merge conflict to reset the conflicted files to their original state 
+
+
+// to delete any branch 
+// git branch -d branch_name 
+
