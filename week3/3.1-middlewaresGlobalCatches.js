162f371d50a52b8277322f7885e6373ffcaeeda1
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-07-28 16:32:51 +0530

    zod examples

--- week3/3.1-middlewaresGlobalCatches.js
+++ week3/3.1-middlewaresGlobalCatches.js
@@ -75,11 +75,46 @@ app.use(function(err, req, res, next){
 const zod = require("zod");
 const schema = zod.array(zod.number()) //this will make sure the input must an array of numbers;
 
+//for the string which look like an email
+    // email:string=>email 
+// password: atleast 8 letters 
+// country: "IN", "US" 
+
+
+
+
 app.post("/", function(req, res){
     const kidney = req.body.kidneys;
     const response = schema.safeParse(kidney) //our input is passed to the schema parse safely and returned in response 
 
-    res.send({
-        response
-    })
+    if(!response.success){
+        res.status(411).json({
+            msg:"input is invalid"
+        })
+    }
+    else{
+        res.send({
+            response
+        })
+    }
+
 })
+
+
+// //you can add multiple things in this object 
+// const schema2 = zod.object({
+//  email:zod.string(),
+//  password:zod.string(),
+//  country:zod.literal("IN").or(zod.literal("US"))
+// })
+
+
+// one more example 
+function validateInput (arr){
+    const schema = zod.array(zod.number());
+    const response= schema.validate(arr);
+    
+    console.log(response);
+}
+
+validateInput([1, 2,3 ,4 ])
