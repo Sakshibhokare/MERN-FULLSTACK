2600a68efc065faa6430c231371d7c2aed2acd24
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-08-28 17:04:22 +0530

    DOM concepts

---
+++ week4/class-4/4.1-domIntro.js
@@ -0,0 +1,8 @@
+// DOM, Dynamic frontends, Connecting FE to BE 
+// DOM: Documents, Object, Model 
+// the DOM is a Programming interface for web document. it represents the page so that programs can change the document structuredClone, style, and content. The Dom represents the document as a tree of objects; each object represents a part of the page. 
+//adding some functionality on the screen is the dynamic development 
+
+
+
+
