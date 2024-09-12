9ce1f16e1cc4b1e0c5ca92dae3b6fcdc50d93bfe
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-09-12 22:43:49 +0530

    course db created

--- assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
@@ -5,12 +5,35 @@ const router = Router();
 //does not mean, this handles the /signup endpoints
 // it will be /admin/signup /admin defined in the main file along with the function for /signup 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username= req.body.username;
+    const password= req.body.password;
+
+    //check if a user with this username already exist
+    await Admin.create({
+        username:username,
+        password:password
+    })
+    res.json({
+        msg:"Admin created successfully"
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title= req.body.title;
+    const description= req.body.description;
+    const imageLink=req.body.imageLink;
+    const price=req.body.price;
+
+    Course.create({
+        title:title,
+        description:description,
+        imageLink:imageLink,
+        price:price
+    })
+
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
