249e57446dc146adf5b5f5e735e35b89b63a13a7
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-09-24 18:29:47 +0530

    zod file created

--- week5/class-5.2/backend/index.js
+++ week5/class-5.2/backend/index.js
@@ -3,4 +3,15 @@ const app=express();
 
 app.use(express.json());
 
+//create zod to validate hence cretaed type.js
+app.post("/todo", function(req, res){
 
+})
+
+app.get("/todos", function(req, res){
+
+})
+
+app.put("/completed", function(req, res){
+
+})
