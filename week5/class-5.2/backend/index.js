6b2872bc5f76030c919762ed0d1609882337cdb3
Sakshibhokare <bhokaresakshi93@gmail.com>
2024-09-26 22:30:33 +0530

    updated

--- week5/class-5.2/backend/index.js
+++ week5/class-5.2/backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app=express();
 
 app.use(express.json());
 
 //create zod to validate hence cretaed type.js
 app.post("/todo", function(req, res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent the wrong inputs",
+        })
+        return;
+    }
+    //put it in mongodb
 
 })
 
@@ -13,5 +23,13 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/completed", function(req, res){
+    const updatedPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatedPayload);
 
+    if(!parsedPayload.success){
+        res.json(411).json({
+            msg:"You sent the wrong inputs"
+        })
+        return; 
+    }
 })
