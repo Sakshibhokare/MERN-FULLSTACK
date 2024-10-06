7f38493e3be86203d78cb8f880be326fc3bf4eda
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2024-10-06 18:22:09 +0530

    backend updated

--- week5/class-5.2/frontend/src/components/CreateTodo.jsx
+++ week5/class-5.2/frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,45 @@
-export function CreateTodo(){
-    return <div>
-        <input style={{padding:10, margin:20}} type="text" placeholder="Title" /> <br /> <br />
-        <input style={{padding:10, margin:20}} type="text" placeholder="description" /> <br /> <br />
+import { useState } from "react"
 
-        <button style={{padding:10, margin:30}}>Add a Todo</button>
+export function CreateTodo(){
+    //props will be passed to parent -> child never to child to parent
+    const [title, setTitle]=useState("");
+    const [description, setDescription] = useState("");
+
+
+    return <div>
+        <input style={{
+            padding:10, 
+            margin:20}} type="text" placeholder="Title" onChange={function(e){
+                //will get the current value of specific dom element it is same is documnet.querySelector().value
+                const value=e.target.value;
+                setTitle(value);
+            }}/> <br /> <br />
+
+
+        <input style={{
+            padding:10, 
+            margin:20}} type="text" placeholder="description"
+            onChange={function(e){
+                const value = e.target.value;
+                setDescription(value);
+            }}/> <br /> <br />
+
+
+
+        <button style={{
+            padding:10, 
+            margin:30}} onClick={()=>{
+                //axios can help you to stringify the json else you have to write it manually
+                fetch("https://localhost:3000/todos",{
+                    method:"POST",
+                    body:JSON.stringify({
+                        title:title,
+                        description: description
+                    }),
+                    headers:{
+                        "content-type":"application/json"  //app will always check the header need to understand the type of data, only mention type of data will be passed else it will be ignored
+                    }
+                })
+            }}>Add a Todo</button>
     </div>
 }
