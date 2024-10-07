c04b8bb28a34eff7e15a2af38f477d4f1e35a78e
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2024-10-07 18:18:21 +0530

    week 6 experiments react concepts

--- week5/class-5.2/frontend/src/components/Todos.jsx
+++ week5/class-5.2/frontend/src/components/Todos.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // todos will be an array 
 // todos =[{
 //     title: hfjefjsgh,
@@ -7,6 +8,7 @@
 export function Todos({todos}){
     return <div>
         {todos.map(function(todo){
+            // eslint-disable-next-line react/jsx-key
             return <div>
                 <h1>{todo.title}</h1>
                 <h2>{todo.description}</h2>
---
+++ week6/class-6.1/info.jsx
@@ -0,0 +1,19 @@
+//A component can only return a single top level xml
+// why? 
+// 1. Makes it easy to reconciliation 
+
+function App(){
+    return(
+        <Header tittle="something"></Header>
+        // <Header tittle="something"></Header>
+
+    )
+}
+
+function Header({title}){
+    return <div>
+        {title}
+    </div>
+}
+
+export default App
