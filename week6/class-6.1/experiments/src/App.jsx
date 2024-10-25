0ab8c8e93de7932ea1647962649cdc13005e339d
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2024-10-25 20:12:20 +0530

    wrapper components started

--- week6/class-6.1/experiments/src/App.jsx
+++ week6/class-6.1/experiments/src/App.jsx
@@ -1,19 +1,25 @@
 //React returns, React re-rendering, key, wrapper components, useEffect, useMemo, useCallback, useRef
+// wrapper components 
 import React, { useState } from 'react'
 
 
 function App() {
-      <CardWrapper innerComponent={TextComponent}></CardWrapper>
+      <CardWrapper innerComponent={<TextComponent></TextComponent>}></CardWrapper>
 }
 
 function TextComponent(){
   return <div>
-
+      hi there
   </div>
 }
 
-function CardWrapper({}){
-
+function CardWrapper({innerComponent}){
+  //create a div which has a border 
+  //and inside the div, renders the prop
+  
+  return <div style={{border:"2px solid black", width:"20px", height:"20px"}}>
+        {innerComponent}
+  </div>
 }
 
 
