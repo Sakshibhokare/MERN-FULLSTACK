8faf8dbbb64476cde896003fc4ac38e1d377da3a
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-02-22 21:47:26 +0530

    updated

--- week5/class-5.2/backend/db.js
+++ week5/class-5.2/backend/db.js
@@ -10,13 +10,13 @@ const mongoose = require("mongoose");
 //create mongodb server and put the url here
 // will keep this link to .env -
 mongoose.connect("mongodb+srv://Sakshibhokare04:Sakshi111@cluster0.rjojvqj.mongodb.net/") //not a right practice to do in real
-const todoSchema=mongoose.Schema({
+const todoSchema = mongoose.Schema({
         title: String,
         description: String,
-        completed: Boolean 
+        completed: Boolean
 })
 
 const todo = mongoose.model('todos', todoSchema);
-module.exports={
+module.exports = {
         todo
 }
