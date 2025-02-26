fb6326afacbf0b02e78fb626c861576c24b6b766
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-02-26 16:47:22 +0530

    hooks in react

--- week6/class-6.1/experiments/src/3wrapperCom.jsx
+++ week6/class-6.1/experiments/src/3wrapperCom.jsx
@@ -1,1 +1,20 @@
-// wrapper components means : a components takes another component as an input 
+// wrapper components means : a components takes
+// another component as an input
+
+// suppose we create a card wrapper in which will take component as a prop
+
+// function CardWrapper({ innerComponent }) {
+//     return <div>{innerComponent}</div>
+// }
+
+// function TextComponent() {
+//     return <div>
+//         hi there
+//     </div>
+// }
+
+// function App(){
+//     return <>
+//     <CardWrapper innerComponent={<TextComponent></TextComponent>}
+//     </>
+// }
---
+++ week6/class-6.1/experiments/src/hooks.jsx
@@ -0,0 +1,5 @@
+// Hooks in react are function that allows you
+// to hook into react state and lifecycle features from function
+// components
+
+// these functions start with use are called hooks 
