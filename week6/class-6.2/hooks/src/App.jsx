162476c901b9b2e1519f837aa4bd931e94909129
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-05 18:06:47 +0530

    useEffect understood

--- week6/class-6.2/hooks/src/App.jsx
+++ week6/class-6.2/hooks/src/App.jsx
@@ -2,30 +2,76 @@ import { useEffect, useState } from 'react';
 import axios from "axios";
 import './App.css'
 
+// function App() {
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs").then(function (res) {
+//       setTodos(res.data.todos)
+//     })
+//   }, []); //this will only run once till any change happen to its state
+
+//   return (
+//     <>
+//       {todos.map((todo) => {
+//         <Todo title={todo.title} description={todo.description}></Todo>
+//       })}
+//     </>
+//   )
+// }
+
+// function Todo(title, description) {
+//   return <div>
+//     <h1> {title} </h1>
+//     {description}
+//   </div>
+// }
+
+
+
+
+//fetch data according to the id, on click buttons 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [selectedID, setSelectedID] = useState(1);
+
+  return <div>
+    <button onClick={function () {
+      setSelectedID(1)
+    }}>1</button>
+    <button onClick={function () {
+      setSelectedID(2)
+    }}>2</button>
+    <button onClick={function () {
+      setSelectedID(3)
+    }}>3</button>
+    <button onClick={function () {
+      setSelectedID(4)
+    }}>4</button>
+    <Todo id={selectedID}></Todo>
+  </div>
+}
+
+function Todo({ id }) {
+  const [todo, setTodos] = useState([]);
 
   useEffect(() => {
-    axios.get("https://sum-server.100xdevs").then(function (res) {
+    axios.get(`https://sum-server.100xdevs/todo?id=${id}`).then(function (res) {
       setTodos(res.data.todos)
     })
-  }, []);
-
+  }, [id]); //id is passed as a dependency when the id get changes this will get rerender
 
   return (
     <>
-      {todos.map((todo) => {
-        <Todo title={todo.title} description={todo.description}></Todo>
-      })}
+      <h1>
+        {todo.title}
+      </h1>
+      <h2>
+        {todo.description}
+      </h2>
     </>
   )
 }
 
-function Todo(title, description) {
-  return <div>
-    <h1> {title} </h1>
-    {description}
-  </div>
-}
+
 
 export default App
