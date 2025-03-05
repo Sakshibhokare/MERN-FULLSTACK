7af3a3498497b88ad2a66676f2f57c85affcf290
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-05 17:37:37 +0530

    useEffect

--- week6/class-6.2/hooks/src/App.jsx
+++ week6/class-6.2/hooks/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs").then(function (res) {
+      setTodos(res.data.todos)
+    })
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map((todo) => {
+        <Todo title={todo.title} description={todo.description}></Todo>
+      })}
     </>
   )
+}
+
+function Todo(title, description) {
+  return <div>
+    <h1> {title} </h1>
+    {description}
+  </div>
 }
 
 export default App
