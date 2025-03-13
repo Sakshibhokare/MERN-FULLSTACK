6040daa45e0dd675ca6d0be74dc872c0a18e9cba
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-13 16:40:37 +0530

    callback assignment

--- week6/class-6.4/assignments/2-use-callback/src/components/Assignment1.jsx
+++ week6/class-6.4/assignments/2-use-callback/src/components/Assignment1.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 
-// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+// Create a counter component with increment and decrement functions. 
+// Pass these functions to a child component which has buttons to 
+// perform the increment and decrement actions. Use useCallback to 
+// ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
     const [count, setCount] = useState(0);
 
-    // Your code starts here
-    function handleIncrement() {
+    //if we add count as dependency then function will rerender because count is changing 
 
-    }
+    const handleIncrement = useCallback(() => {
+        setCount(count + 1);
+    }, [])
 
-    function handleDecrement() {
-    }
+
+
+    const handleDecrement = useCallback(() => {
+        setCount(count - 1);
+    }, [])
+
+
     // Your code ends here
 
     return (
@@ -20,11 +29,19 @@ export function Assignment1() {
             <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
         </div>
     );
-};
+}
 
-const CounterButtons = ({ onIncrement, onDecrement }) => (
-    <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
-    </div>
-);
+// eslint-disable-next-line react/display-name
+const CounterButtons = memo(
+    // eslint-disable-next-line react/prop-types
+    ({ onIncrement, onDecrement }) => (
+        <div>
+            <button onClick={onIncrement}>Increment</button>
+            <button onClick={onDecrement}>Decrement</button>
+        </div>
+    )
+)
+
+//memo and callbacks both should be used 
+// memo will tell you that props are not changed 
+// useCallback will handle rerendering when passing function in component 
