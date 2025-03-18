1ed0bc0e7d10340896ecf9e3b62727b9dab24357
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-18 22:09:09 +0530

    react-router-dom

--- week7/class-7.1/routing_propdrilling_ContextApi.jsx
+++ week7/class-7.1/routing_propdrilling_ContextApi.jsx
@@ -1,4 +1,18 @@
 // Routing
 // - Single page application: react let's you create single page app
 // - Client side bundle
-// - client side routing 
+// - client side routing: the code that u write
+
+// routes
+// we have react-router-dom
+// this library help for client side routing 
+
+// basic routing 
+function App() {
+    <BrowserRouter>
+        <Routes>
+            <Route path="/dashboard" element={<Dashborad></Dashborad>}></Route>
+            <Route path="/" element={<Landing></Landing>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
