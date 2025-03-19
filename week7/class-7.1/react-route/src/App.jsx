91b631c2c0b2347190d954471e396f3f5b02e004
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-19 21:58:19 +0530

    routing

--- week7/class-7.1/react-route/src/App.jsx
+++ week7/class-7.1/react-route/src/App.jsx
@@ -6,14 +6,18 @@ import { Landing } from './components/Landing';
 function App() {
 
   return (
-    <>
+    <div>
+      {/* if want something to stay on every route  */}
+      <div style={{ backgroundColor: 'black', color: "white" }}>
+        Hi this is the topbar
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
           <Route path="/" element={<Landing></Landing>}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
