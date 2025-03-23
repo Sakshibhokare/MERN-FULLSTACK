af9b8b39b32f8313d25bb543df8dc3d2774eb8eb
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-03-23 23:33:54 +0530

    context API

--- week7/class-7.1/react-route/src/components/zContextAPI.jsx
+++ week7/class-7.1/react-route/src/components/zContextAPI.jsx
@@ -1,2 +1,50 @@
 // Context APIS
 // it helps in the problem of prop drilling 
+import { useContext, useState } from "react"
+import { CountContext } from "../context";
+
+function App() {
+    const [count, setCount] = useState(0);
+    return (
+        <>
+            <CountContext.Provider value={count}>
+                <Count setCount={setCount}></Count>
+            </CountContext.Provider>
+            <Count count={count} setCount={setCount}></Count>
+            {/* <Buttons count={count} setCount={setCount}></Buttons> */}
+        </>
+    )
+}
+
+
+function Count({ count, setCount }) {
+    return <>
+        <CountRerender></CountRerender>
+        <Buttons count={count} setCount={setCount}></Buttons>
+    </>
+}
+
+function CountRerender() {
+    const count = useContext(CountContext); //react gives us a hook useContext, using it we can access the value, without passing as a prop
+    return <>
+        {count}
+    </>
+}
+function Buttons(setCount) {
+    const count = useContext(CountContext);
+    return <>
+        <button onClick={() => {
+            setCount(count + 1);
+        }}>
+            Increase
+        </button>
+
+        <button onClick={() => {
+            setCount(count - 1);
+        }}>
+            Decrease
+        </button>
+    </>
+}
+
+export default App
---
+++ week7/class-7.1/react-route/src/context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+//we have create the context to use state variable 
+// across various components 
+// once you create this you need to wrap component inside this context 
+// to use state variable 
+export const CountContext = createContext(0);
