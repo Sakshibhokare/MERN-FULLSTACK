e97ebed5236063f263676344732512bcedde8016
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-02 19:16:25 +0530

    state management using recoil

--- week7/class-7.2/stateManagementRecoil.jsx
+++ week7/class-7.2/stateManagementRecoil.jsx
@@ -1,2 +1,11 @@
 // what is state management
-// A cleaner way to store state of your app 
+// A cleaner way to store state of your app
+
+// we define all components is separate folder and
+// all state in separate folder
+
+// popular state management libraries
+// Recoil , Zustand, redux
+
+// recoil has the concept of an atom
+// an atom can be defined outside the component 
