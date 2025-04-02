294e1010fd69515cfe6b819b3e3084ab0a8d673d
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-02 19:05:55 +0530

    state management recoil

--- week7/class-7.2/context.jsx
+++ week7/class-7.2/context.jsx
@@ -2,4 +2,15 @@
 
 // steps :
 // 1st : define the context in a file outside the main file
-// 2nd : use useContext(variableName0) to use the value of that context 
+//give that context a value using <.provider value={count}> in the main file
+// 2nd : use useContext(variableName0) to use the value of that context
+
+// vvv imp interview q
+// why do you use the context API to make rendering more performing the answer is no
+// we use only to make syntax cleaner and to get rid of prop drilling
+// when you are not using any context variable then that component should not rerender
+// but is rerendering
+
+// to make it effective for rerendering
+// we have state management libraries like recoil
+
---
+++ week7/class-7.2/stateManagementRecoil.jsx
@@ -0,0 +1,2 @@
+// what is state management
+// A cleaner way to store state of your app 
