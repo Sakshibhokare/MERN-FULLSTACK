3a6d071c07908c3fa772b1d82e7c98f976b508a4
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-02 18:25:30 +0530

    context understanding

--- week7/class-7.2/context.jsx
+++ week7/class-7.2/context.jsx
@@ -1,1 +1,5 @@
 //context: we define the variable outside, then we use that variable directly, without passing as a prop
+
+// steps :
+// 1st : define the context in a file outside the main file
+// 2nd : use useContext(variableName0) to use the value of that context 
