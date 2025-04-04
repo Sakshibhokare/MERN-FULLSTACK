9e22a1e229035c1c2a9214b5bedb9a01b7de05ff
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-04 18:23:15 +0530

    updated

---
+++ week7/class-7.4.0/7.4/7.4-easy-bits/src/App.jsx
@@ -0,0 +1,50 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState, } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
+
+//RecoilRoot
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationsAtomCount = useRecoilValue(notificationsAtom)
+  const messagingAtomCount = useRecoilValue(messagingAtom)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  // const totalNotificationCount = useMemo(() => {
+  //   return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
+  // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount, messagingAtomCount]) 
+
+  return (
+    <>
+      <button>Home</button>
+
+      <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs {jobsAtomCount}</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+
+      <ButtonUpdater></ButtonUpdater>
+    </>
+  )
+}
+
+function ButtonUpdater() {
+  const setMessageAtomCount = useSetRecoilState(messagingAtom);
+  return <button onClick={() => {
+    setMessageAtomCount(c => c + 1);
+  }}>
+
+  </button>
+}
+export default App
