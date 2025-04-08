ef0a523b75a161e57a69620ba21df9c7f4cc5a80
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-08 12:28:29 +0530

    tailwind css

--- week7/class-7.4.0/7.4/loadables/src/App.jsx
+++ week7/class-7.4.0/7.4/loadables/src/App.jsx
@@ -5,18 +5,25 @@ import { todosAtomFamily } from './atoms';
 
 function App() {
   return <RecoilRoot>
-    <Todo id={1}/>
+    <Todo id={1} />
     <Todo id={2} />
   </RecoilRoot>
 }
 
-function Todo({id}) {
-   const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
-   if (todo.state === "loading") {
-      return <div>loading</div>
-   }
-   
-   return (
+function Todo({ id }) {
+  //useRecoilStateLoadable: this returns if the state is resolved or not 
+  const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
+  // const [todos, setTodos] = useRecoilValueLoadable(todosAtomFamily(id));
+
+  if (todo.state === "loading") {
+    return <div>loading...</div>
+  } else if (todo.state === "hasError") {
+    return <>
+      Error while getting the data
+    </>
+  }
+
+  return (
     <>
       {todo.contents.title}
       {todo.contents.description}
