6b50c32176a3e930276ae4fc50c0db87c44e1dd9
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-22 18:34:13 +0530

    tailwind css practice

--- week8/class-8.1-latest/youtube_clone/src/components/VideoCard.tsx
+++ week8/class-8.1-latest/youtube_clone/src/components/VideoCard.tsx
@@ -1,13 +1,24 @@
-export function VideoCard() {
-    return <div>
-        <img src="/thumbnail.jpg"></img>
-        <div className="grid grid-cols-12 bg-red-50">
-            <div className="grid-span-2">
-                <img src="/thumbnail.jpg"></img>
+export function VideoCard(props: any) {
+    return <div className="pl-2">
+        <img src={props.thumbnail}></img>
+        <div className="grid grid-cols-12 ">
+            <div className="col-span-1">
+                <img className="rounded-full w-15 h-15 " src={props.image}></img>
             </div>
-            <div className="grid-span-10">
-                Title: demo title
+
+            <div className="col-span-11 pl-2">
+                <div>
+                    {props.title}
+                </div>
+                <div className="col-span-10 pl-5 text-gray-600 text-base">
+                    {props.author}
+                </div>
+                <div className="col-span-10 pl-5 text-gray-600 text-base">
+                    {props.views} | {props.timestamp}
+                </div>
+
             </div>
+
         </div>
     </div>
 }
--- week8/class-8.1-latest/youtube_clone/src/pages/index.tsx
+++ week8/class-8.1-latest/youtube_clone/src/pages/index.tsx
@@ -6,7 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <VideoCard></VideoCard>
+      <VideoCard
+        title={"how to learn coding in 30 days"}
+        image={"thumbnail.jpg"}
+        thumbnail={"thumbnail.jpg"}
+        author={"Saksheee"}
+        views={"100k"}
+        timestamp={"2 days ago"}></VideoCard>
     </div>
   );
 }
