7e3792fff7da45131c9127f7a25ef332c530b887
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-04-28 22:24:34 +0530

    updated

--- week8/class-8.1-latest/youtube_clone/src/components/Appbar.tsx
+++ week8/class-8.1-latest/youtube_clone/src/components/Appbar.tsx
@@ -2,13 +2,13 @@ import { SearchBar } from "./SearchBar"
 
 export const AppBar = () => {
     return <div className="grid grid-cols-3 p-5" >
-        <div>
+        <div className="text-md inline-flex items-center pb-2">
             YouTube
         </div>
         <div>
             <SearchBar></SearchBar>
         </div>
-        <div>
+        <div className="text-md inline-flex items-center pb-2">
             sign in
         </div>
     </div>
