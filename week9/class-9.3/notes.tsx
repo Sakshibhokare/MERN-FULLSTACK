77026f4c66f7daf30049035c8a8d8cbb50e5c414
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-06-12 17:46:16 +0530

    generics

--- week9/class-9.3/notes.tsx
+++ week9/class-9.3/notes.tsx
@@ -1,4 +1,4 @@
-// enum example 
+// 1. enum example 
 enum directions {
     Up,
     Down,
@@ -10,7 +10,7 @@ enum directions {
 // this helps in preventing the code changes at multiple places
 // and improves the code readability
 
-// Generics
+// 2. Generics
 // lets say you have a function that needs to return the first element of an array
 // Array can be of type either string or integer 
 
@@ -18,3 +18,11 @@ type Input = number | string
 function firstEl(arr: Input[]) {
     return arr[0];
 }
+
+//if i want to convert the string into uppercase
+const value = firstEl(["hello"]);
+// value.toUpperCase() // is not possible here because the value can be string or number
+
+// if you want that the array should only contain
+// string or numbers then we can use generics
+// generics enable u to create component that work with any type while still providing compile time type safety 
