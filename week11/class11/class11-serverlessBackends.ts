4467e162164bbe751607ba4b8187018f12c7d9ed
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-06-18 17:52:36 +0530

    cloudflare

---
+++ week11/class11/class11-serverlessBackends.ts
@@ -0,0 +1,19 @@
+// Serverless backend
+// 1. what are backend servers
+// serverless is a backend deployment in which the cloud provider dynamically manages the allocation and provisioning of servers.
+// the term serverless doesn't mean there are no server involved
+// instead it mean that developers and operators do not have to worry about the servers
+
+//if the deployment is not serverless then if the server gets down then developer will be creating server, which mean someone will always taking care of it which is not good
+
+//benefits of serverless : if no one is coming in your website then no server is allocated and no charge get applied,
+// even if no one coming for 2 hrs on your website, no server is allocated till then
+
+// famous serverless providers
+// AWS Lambda
+// google Cloud functions
+// Cloudflare workers
+
+//2. how cloudflare workers works: cloudflare workers do not use nodejs runtime, not bun
+// they have create there own runtime v8 engine 
+
