9403337afdd95c18c9d97c5951402e77cc16e502
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-07-18 18:30:29 +0530

    prisma setup done

---
+++ week12/12.5_Prisma_recap_relationshipsInPrisma/dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prisma_1 = require("./generated/prisma");
+const prisma = new prisma_1.PrismaClient();
+//this will generate the sql query, this is similar for any database
+function insertUser(username, password, firstname, lastname) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield prisma.user.create({
+            data: {
+                username,
+                password,
+                firstname,
+                lastname,
+            }
+        });
+    });
+}
+insertUser("demo", "demo", "demo", "demo");
---
+++ week12/12.5_Prisma_recap_relationshipsInPrisma/src/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "./generated/prisma";
+
+const prisma = new PrismaClient();
+
+//this will generate the sql query, this is similar for any database
+async function insertUser(username:string, password:string, firstname:string, lastname:string) {
+    const response = await prisma.user.create({
+        data:{
+            username,
+            password,
+            firstname,
+            lastname,
+
+        }
+    })
+}
+
+insertUser("demo", "demo", "demo", "demo");
