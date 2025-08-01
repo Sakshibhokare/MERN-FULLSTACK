a31eec113cbba3ffb8d7a33ca5e5b4854aaef0ae
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-08-01 16:11:20 +0530

    medium backend

--- node_modules/hono/dist/types/adapter/bun/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module
- * Bun Adapter for Hono.
- */
-export { serveStatic } from './serve-static';
-export { bunFileSystemModule, toSSG } from './ssg';
-export { createBunWebSocket } from './websocket';
-export type { BunWebSocketData, BunWebSocketHandler } from './websocket';
-export { getConnInfo } from './conninfo';
