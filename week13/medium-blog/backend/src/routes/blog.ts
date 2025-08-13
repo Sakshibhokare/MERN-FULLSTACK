48086da0195f75f820b01c01e2e998215c2ba34e
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-08-13 18:43:46 +0530

    blog routes created

--- week13/medium-blog/backend/src/routes/blog.ts
+++ week13/medium-blog/backend/src/routes/blog.ts
@@ -1,16 +1,63 @@
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 
-export const blogRouter = new Hono();
+export const blogRouter = new Hono<{
+    Bindings:{
+        DATABASE_URL: String;
+        JWT_SECRET: String
+    }
+}>();
 
-blogRouter.post('/blog', (c) => {
+
+blogRouter.use("/*", (c, next)=>{
+    //extract the user id 
+    //pass it down to the route handler 
+    next();
+})
+
+blogRouter.post('/', async(c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate);
+
+    const blog = await prisma.blog.create({
+        data:{
+            title:body.title,
+            content: body.content,
+            authorId: 1
+        }
+    })
+  return c.json({
+    id:blog.id
+  })
+})
+
+blogRouter.put('/', async(c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate);
+
+    const blog = await prisma.blog.update({
+        where:{
+            id:body.id
+        },
+        data:{
+            title:body.title,
+            content: body.content,
+            authorId: 1
+        }
+    })
+  return c.json({
+    id:blog.id
+  })
+
   return c.text('Hello Hono!')
 })
 
-blogRouter.put('/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-blogRouter.get('/blog/:id', (c) => {
+blogRouter.get('/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
