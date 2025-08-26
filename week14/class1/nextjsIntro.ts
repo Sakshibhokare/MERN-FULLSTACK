a796d2b59fbbca81dc9dc41d9fa7146893130016
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-08-26 18:57:59 +0530

    started with next js intro

--- week14/class1/nextjsIntro.ts
+++ week14/class1/nextjsIntro.ts
@@ -1,1 +1,6 @@
 // step 1 - next js intro 
+// why next: 
+// react and backend stays seprately 
+// react does not provide out the box routing (you have to use react-router-dom)
+// 3. react is not SEO Optimised
+// 4. waterfalling problem 
